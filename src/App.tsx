74f43333b0022ff2f9be556cecb342f66ce64ef5
Gabriel <105824688+Custodio97@users.noreply.github.com>
2022-08-25 21:25:30 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Widget } from "./components/Widget"
+import './global.css'
+function App() {
+
+
+  return (
+    <div className="App">
+      <Widget/>
+    </div>
+  )
+}
+
+export default App
